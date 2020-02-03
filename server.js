ad79af75b87ed2c3e322c6e0368de1e698eaabd8
Cecilia <cecilia.gaetarelli@gmail.com>
2020-02-03 17:27:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express');
 // create the app
 var app = express();
+
+let Player = require("./Player");
 // define the port where client files will be provided
 var port = process.env.PORT || 3000;
 // start to listen to that port
@@ -40,3 +42,32 @@ function newConnection(socket) {
   }
 }
 console.log('node server is running')
+
+let io = socket(server);
+
+let players = [];
+
+setInterval(updateGame, 16);
+
+io.sockets.on("connection", socket => {
+  console.log(`New connection ${socket.id}`);
+  players.push(new Player(socket.id));
+
+  socket.on("disconnect", () => {
+    io.sockets.emit("disconnect", socket.id);
+    players = players.filter(player => player.id !== socket.id);
+  });
+});
+
+
+io.sockets.on("disconnect", socket => {
+  io.sockets.emit("disconnect", socket.id);
+
+  players = players.filter(player.id !== socket.id);
+});
+
+
+
+function updateGame() {
+  io.sockets.emit("heartbeat", players);
+}
