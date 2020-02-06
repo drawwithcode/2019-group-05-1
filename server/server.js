f8839975a118d38bcaf2aeb6331f25bd73638817
Bianca Cheung <biancacheung1@hotmail.com>
2020-02-06 14:58:07 +0100

    server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 // load express library
 var express = require('express');
+//total score for this session
+let score=0;
 // create the app
 var app = express();
-
-let Player = require("./Player");
 // define the port where client files will be provided
 var port = process.env.PORT || 3000;
 // start to listen to that port
@@ -22,52 +22,27 @@ io.on('connection', newConnection);
 // will contain all the information on the new connection
 function newConnection(socket) {
   //when a new connection is created, print its id
-  console.log('socket:', socket.id);
+ // console.log('socket:', socket.id);
   //define what to do on different kind of messages
   socket.on('newCrack', newCrack);
   socket.on('deleteCrack', eraseCrack);
   socket.on('sendPosition', sendPosition);
+  socket.on('increaseScore', updateTotalScore);
+
 	//reaction on messages
   function newCrack(crackData) {
     socket.broadcast.emit('crackBroadcast', crackData);
-    console.log(crackData);
   }
   function eraseCrack(deleteId) {
     socket.broadcast.emit('deleteBroadcast', deleteId);
-    console.log(deleteId);
   }
   function sendPosition(localPlayer) {
     socket.broadcast.emit('receivePosition', localPlayer);
-    console.log(localPlayer);
+  }
+  function updateTotalScore(){
+    score+=1;
+    io.emit('updateTotal', score);
+
   }
 }
-console.log('node server is running')
-
-let io = socket(server);
-
-let players = [];
-
-setInterval(updateGame, 16);
-
-io.sockets.on("connection", socket => {
-  console.log(`New connection ${socket.id}`);
-  players.push(new Player(socket.id));
-
-  socket.on("disconnect", () => {
-    io.sockets.emit("disconnect", socket.id);
-    players = players.filter(player => player.id !== socket.id);
-  });
-});
-
-
-io.sockets.on("disconnect", socket => {
-  io.sockets.emit("disconnect", socket.id);
-
-  players = players.filter(player.id !== socket.id);
-});
-
-
-
-function updateGame() {
-  io.sockets.emit("heartbeat", players);
-}
+console.log('node server is running on port'+port)
